9cd672a0311b87f173cfc0229426190a06653cb3
unknown <you@example.com>
2021-03-19 09:11:13 +0700

    first commit

---
+++ AddOnLimitTransaction/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using CXS.Retail.Extensibility;
+using System;
+
+namespace AddonPrintTransaction
+{
+    class Plugin : BasePlugin // nếu để public class (version 6.6) khi restart MC sẽ bị lỗi
+    {
+        public override string Name
+        {
+            get { return "AddonPrintTransaction"; }
+        }
+
+        public override string Description
+        { 
+            get { return "Addon Print Transaction"; }
+        }
+
+        public override string CompanyName
+        {
+            get { return "FTI Trading"; }
+        }
+
+        public override Version VersionInfo
+        {
+            get { return new Version("1.0.0.0"); }
+        }
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
---
+++ AddOnLimitTransaction/Readme.txt
@@ -0,0 +1,7 @@
+﻿1. NỘI DUNG: Điều chỉnh số lần in ra bill theo số lần giới hạn do người dùng cài đặt trong phần Enterprise
+2. Điều kiện tiên quyết 
+Truy cập theo đường dẫn để tạo UDF:  Administration > Application Setup > User Defined Fields. 
+- Object Type: chọn	Enterprise
+- Field Name: Đặt U_LimitPrintNumber
+- Check vào: Display in Results(Cho phép trường tạo hiển thị trong Enterprise)
+- Mở Enterprise nhập số lượng giới hạn in
