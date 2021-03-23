f166b915f9859b3d35cdea7bf426b29c57acb6ef
unknown <you@example.com>
2021-03-23 09:24:48 +0700

    nhanh moi

---
+++ AddOnLimitTransaction/Log/LogType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOnLimitTransaction.Log
+{
+    public enum LogType
+    {
+        INFOR,
+        WARRING,
+        ERROR
+    }
+}
